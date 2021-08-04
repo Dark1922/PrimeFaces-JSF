29f8a4e91fd4d488a95f745eef5c476accf404be
dark1922 <jvdematos004@gmail.com>
2021-08-03 21:51:21 -0300

    feath: cep em primefaces

--- PrimeFacesJSF/src/main/java/managedBean/UsuarioPessoaManagedBean.java
+++ PrimeFacesJSF/src/main/java/managedBean/UsuarioPessoaManagedBean.java
@@ -1,5 +1,10 @@
 package managedBean;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,9 +12,12 @@ import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlInputText;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
 
-import br.com.dao.DaoGeneric;
+import com.google.gson.Gson;
+
 import br.com.dao.DaoUsuario;
 import br.com.model.UsuarioPessoa;
 
@@ -62,6 +70,52 @@ public class UsuarioPessoaManagedBean {
 		}
 		return "";
 
+	}
+	
+	
+	public void pesquisaCep(AjaxBehaviorEvent event) {// tem que tar declarado aqui pro jsf entender o listener
+
+		try {
+			// oque tiver sendo enviado pela parte do html do cep vai está sendo enviado
+			// para o inputText e pegamos ele dai
+			// e passa ele pra buscar na url de acordo com oque o usuário escreveu
+			HtmlInputText inputText = (HtmlInputText) event.getSource(); // vai converter pra um input text html
+
+			// linkg do ibg que retorna um json pelo cep passado se ele for válido passando
+			// o cep que foi passado por parametro
+			URL url = new URL("https://viacep.com.br/ws/" + inputText.getValue() + "/json/");// monta a url
+			URLConnection connectionDaUrl = url.openConnection(); // abre a conexão
+			InputStream is = connectionDaUrl.getInputStream(); // vai executar ao lado do servidor e vai retornar os
+																// dados / retorno
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8")); // faz a leitura desse retorno
+
+			String cep = ""; //váriavel auxiliar
+			StringBuilder jsonCep = new StringBuilder();
+			while ((cep = br.readLine()) != null) { // vai varrer as linha eqnt for diferente de null
+				jsonCep.append(cep); //vai adicionando oq tiver no cep logadouro etcq
+			}
+			// vai jogar os dados pro objeto logadouro etc os dados que vem com cep pelos
+			// objetos criado em Pessoa
+			UsuarioPessoa gsonAux = new Gson().fromJson(jsonCep.toString(), UsuarioPessoa.class);
+
+			// setando os dados pelo gson auxiliar
+			usuarioPessoa.setCep(gsonAux.getCep());
+			usuarioPessoa.setLogradouro(gsonAux.getLogradouro());
+			usuarioPessoa.setComplemento(gsonAux.getComplemento());
+			usuarioPessoa.setBairro(gsonAux.getBairro());
+			usuarioPessoa.setLocalidade(gsonAux.getLocalidade());
+			usuarioPessoa.setUf(gsonAux.getUf());
+			usuarioPessoa.setUnidade(gsonAux.getUnidade());
+			usuarioPessoa.setIbge(gsonAux.getIbge());
+			usuarioPessoa.setGia(gsonAux.getGia());
+
+			// System.out.println(jsonCep);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
+					"Informação: ", "Cep Inválido"));
+		}
 	}
 
 	// get da nossa lista
